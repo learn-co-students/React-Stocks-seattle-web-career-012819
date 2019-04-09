f9cd245a7b5973fa3f865ec5e276c1036f46b37b
Jessica Betts <jessicaannbettsis@gmail.com>
2019-04-09 16:56:28 -0700

    Done.

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,23 +1,23 @@
 import React from 'react';
 
-const SearchBar = () => {
+const SearchBar = (props) => {
   return (
     <div>
 
       <strong>Sort by:</strong>
       <label>
-        <input type="radio" value="Alphabetically" checked={null} onChange={null}/>
+        <input type="radio" value="Alphabetically" checked={null} onChange={props.sortStockName}/>
         Alphabetically
       </label>
       <label>
-        <input type="radio" value="Price" checked={null} onChange={null}/>
+        <input type="radio" value="Price" checked={null} onChange={props.sortStockPrice}/>
         Price
       </label>
       <br/>
 
       <label>
         <strong>Filter:</strong>
-        <select onChange={null}>
+        <select onChange={props.filterStock}>
           <option value="Tech">Tech</option>
           <option value="Sportswear">Sportswear</option>
           <option value="Finance">Finance</option>
--- src/containers/PortfolioContainer.js
+++ src/containers/PortfolioContainer.js
@@ -2,13 +2,24 @@ import React, { Component } from 'react';
 import Stock from '../components/Stock'
 
 class PortfolioContainer extends Component {
+  constructor(props){
+    super(props)
+
+  }
+
+  displayStocks = () => {
+    //console.log(this.props.purchased)
+    return this.props.purchased.map(stock => {
+        return <Stock key={stock.id} stock={stock} handleStock={this.props.handleStock} />
+    })
+  }
 
   render() {
     return (
       <div>
         <h2>My Portfolio</h2>
           {
-            //render your portfolio stocks here
+            this.displayStocks()
           }
       </div>
     );
